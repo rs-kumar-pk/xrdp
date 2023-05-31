95bfb98ce4896f67613375d32a4a9baf8c0a6a56
rs-kumar-pk <123354372+rs-kumar-pk@users.noreply.github.com>
2023-05-31 17:02:06 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+
+
+#!/bin/bash
+
+sudo mkfs.xfs /dev/sdc
+
+d=$(blkid -s UUID -o value /dev/sdc)
+
+sudo mkdir /data
+
+sudo su -c "echo UUID=$d /data xfs defaults 0 0 >> /etc/fstab"
+
+sudo mount /data
+
+sudo timedatectl set-timezone Canada/Eastern
+sudo apt-get update -y
+sudo apt-get install zip unzip
+sudo apt install net-tools
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4
+		sudo apt install xfce4-session
+		sudo apt-get -y install xrdp
+		sudo systemctl enable xrdp
+		sudo adduser xrdp ssl-cert
+		echo xfce4-session >~/.xsession
+		sudo service xrdp restart
+
+sudo echo "Updating system..."
+sudo apt-get update -y
+sudo apt-get upgrade -y
