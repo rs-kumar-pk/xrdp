6a7c6288803210cd8c7010b2fd7d0efc74f4cb1f
rs-kumar-pk <123354372+rs-kumar-pk@users.noreply.github.com>
2023-05-31 17:39:00 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,12 +17,12 @@ sudo apt-get update -y
 sudo apt-get install zip unzip
 sudo apt install net-tools
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4
-		sudo apt install xfce4-session
-		sudo apt-get -y install xrdp
-		sudo systemctl enable xrdp
-		sudo adduser xrdp ssl-cert
-		echo xfce4-session >~/.xsession
-		sudo service xrdp restart
+sudo apt install xfce4-session
+sudo apt-get -y install xrdp
+sudo systemctl enable xrdp
+sudo adduser xrdp ssl-cert
+echo xfce4-session >~/.xsession
+sudo service xrdp restart
 
 sudo echo "Updating system..."
 sudo apt-get update -y
